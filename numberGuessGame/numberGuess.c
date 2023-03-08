e2c0d6efe0e93ed498fc2360bfc90c9cf2433a22
cawauchi6204 <cawauchi6204@gmail.com>
2023-03-08 20:33:29 +0900

    update: 入力回数制限をつけた

--- numberGuessGame/numberGuess.c
+++ numberGuessGame/numberGuess.c
@@ -5,25 +5,34 @@
 int main(void)
 {
   srand(time(NULL));
-  printf("0~999の整数を当てよう!\n\n");
+
+  const int max_stage = 10;
+  int remain = max_stage;
 
   int ans = rand() % 1000;
   int no;
 
+  printf("0~999の整数を当てよう!\n\n");
   // ここは無限ループだけどbreak文があるからそれで抜けられる
-  while (1)
+  do
   {
     printf("いくつかな:");
     scanf("%d", &no);
+    remain--;
 
     if (no > ans)
       printf("もっと小さいよ。\a\n");
     if (no < ans)
       printf("もっと大きいよ。\a\n");
-    if (no == ans)
-      break;
-  };
-  printf("正解です。\n");
+  } while (no != ans && remain > 0);
+
+  if (no != ans)
+    printf("残念。正解は%dでした", ans);
+  else
+  {
+    printf("正解です。\n");
+    printf("%d回で当たりましたね。\n", max_stage - remain);
+  }
 
   return 0;
 }
